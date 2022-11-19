9fe4f867d0a40dd03e66494037e2990784c43100
Havok <lucas.cox.dev@gmail.com>
2022-11-19 17:09:54 +0100

    fix(brain): check if slot is empty before playing on it

--- brain.py
+++ brain.py
@@ -3,7 +3,12 @@ from game import Game
 from random import randint
 
 
+def generate_random_coordinates(length: int, height: int) -> (int, int):
+    return randint(0, length - 1), randint(0, height - 1)
+
 def evaluate(game: Game) -> (int, int):
-    x: int = randint(0, game.board.length - 1)
-    y: int = randint(0, game.board.height - 1)
+    x: int = -1
+    y: int = -1
+    while x >= 0 and y >= 0 and game.board.stones[x][y] != 0:
+        x, y = generate_random_coordinates(length, height)
     return x, y
