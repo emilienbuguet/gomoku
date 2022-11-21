bb6cd426d59d53ca0535368cb5cf6f3d3be2f695
Havok <lucas.cox.dev@gmail.com>
2022-11-21 19:54:26 +0100

    feat(brain): slow implementation of MonteCarlo

--- win.py
+++ win.py
@@ -26,28 +26,70 @@ def check_diag(board: Board, x: int, y: int, direction: str, player) -> (int, in
     return -1, -1
 
 
-def win_coordinates(game: Game, player: Players) -> (int, int):
+def win_coordinates(board: Board, player: Players) -> (int, int):
     """Returns coordinates of the winning move for the given player.
     :returns: (x, y) or (-1, -1) if there is no winning move.
     """
-    for y in range(game.board.height):
-        x = check_line(game.board[y], player)
+    for y in range(board.height):
+        x = check_line(board[y], player)
         if x != -1:
             return x, y
 
-    for x in range(game.board.length):
-        col = [row[x] for row in game.board.stones]
+    for x in range(board.length):
+        col = [row[x] for row in board.stones]
         y = check_line(col, player)
         if y != -1:
             return x, y
 
-    for row in range(game.board.height):
-        for col in range(game.board.length):
-            if game.board[row][col] == player.value:
-                x, y = check_diag(board=game.board, x=col, y=row, direction='right', player=player)
+    for row in range(board.height):
+        for col in range(board.length):
+            if board[row][col] == player.value:
+                x, y = check_diag(board=board, x=col, y=row, direction='right', player=player)
                 if x != -1 and y != -1:
                     return x, y
-                x, y = check_diag(board=game.board, x=col, y=row, direction='left', player=player)
+                x, y = check_diag(board=board, x=col, y=row, direction='left', player=player)
                 if x != -1 and y != -1:
                     return x, y
     return -1, -1
+
+
+def has_won(board: Board, player: Players) -> bool:
+    """Checks if player has won the game"""
+    def need_to_check_diag(x: int, y: int) -> bool:
+        if x > 0 and y > 0 and board[y - 1][x - 1] == player.value:
+            if x < board.length - 1 and board[y - 1][x + 1] == 0:
+                return True
+            return False
+        if x < board.length - 1 and y > 0 and board[y - 1][x + 1] == player.value:
+            if x > 0 and board[y - 1][x - 1] == 0:
+                return True
+            return False
+        return True
+
+    def generate_diags(x: int, y: int) -> (list, list):
+        right = list()
+        left = list()
+        if y < board.height - 5 and x < board.length - 5:
+            right = [board[y + i][x + i] for i in range(5)]
+        if y < board.height - 5 and x >= 4:
+            left = [board[y + i][x - i] for i in range(5)]
+        return right, left
+
+    # Row check
+    for row in board.stones:
+        if str(player.value) * 5 in ''.join(list(map(str, row))):
+            return True
+    # Col check
+    cols = [[row[x] for row in board.stones] for x in range(board.length)]
+    for col in cols:
+        if str(player.value) * 5 in ''.join(list(map(str, col))):
+            return True
+    # Diag check
+    for y in range(board.height):
+        for x in range(board.length):
+            if board[y][x] == player.value and need_to_check_diag(x, y):
+                (right, left) = generate_diags(x, y)
+                if [player.value] * 5 == right or [player.value] * 5 == left:
+                    return True
+
+    return False
