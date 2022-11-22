6e804a124abfafc0a5bad8294547e3c9a0c0b515
Lucas Cox <lucas.cox@epitech.eu>
2022-11-22 17:36:14 +0100

    feat(brain): add some pruning on the legal moves getter

--- sources/brain.py
+++ sources/brain.py
@@ -9,13 +9,27 @@ def generate_random_coordinates(length: int, height: int) -> (int, int):
 
 
 def minimax(game: Game) -> (int, int):
+    # Génération d'un game tree sur une depth donnée
+    legal_moves: list = []
     return generate_random_coordinates(game.board.length, game.board.height)
+
+
+def pruned_legal_moves(board: Board) -> list:
+    free_slots: iter = filter(lambda c: c is not None, list([
+        (x, y) if board[y][x] == 0 else None for x in range(board.length) for y in range(board.height)
+    ]))
+    # TODO filter the boxes that are adjacent to already set stones
+    return list()
 
 
 def evaluate(game: Game) -> (int, int):
     x, y = win_coordinates(game.board, Players['ME'])
     if x == -1:
         x, y = win_coordinates(game.board, Players['ENNEMY'])
+    free_slots = pruned_legal_moves(game.board)
+    # TODO check if this is the beginning of the game, if yes play center or random
+    winner = [-1, -1, ]
     while x < 0 and y < 0 or game.board.stones[y][x] != 0:
-        x, y = minimax(game)
+        for (x, y) in free_slots:
+            x, y = minimax(game)
     return x, y
