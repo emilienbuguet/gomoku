0cc305a8348d7ec0c30a14e08a1bd693decb4290
emilienbuguet <emilien.buguet@epitech.eu>
2022-11-28 15:50:34 +0100

    feat(minimax): basic implemtation of minimax algo without prunning

--- sources/brain.py
+++ sources/brain.py
@@ -1,60 +1,16 @@
 """Here is the main loop of the brain algorithm"""
 
-from copy import deepcopy
 from random import randint
-from .game import Game, ME, ENEMY, Board
-from .win import has_won
-from .minimax import minimax
+from .game import Game, ME
+from .minimax import minimax, pruned_legal_moves
 
 
 def generate_random_coordinates(length: int, height: int) -> tuple:
     """Generate a couple of (x, y) coordinates between 0 and length or height"""
     return randint(0, length - 1), randint(0, height - 1)
 
-
-def has_stone_nearby(board: Board, node_x: int, node_y: int) -> bool:
-    """Checks if a node of the board has a stone around it"""
-    top_left_x = node_x - 1 if node_x > 0 else node_x
-    top_left_y = node_y - 1 if node_y > 0 else node_y
-    bottom_right_x = node_x + 1 if node_x < board.length - 1 else node_x
-    bottom_right_y = node_y + 1 if node_y < board.height - 1 else node_y
-
-    for row in board[top_left_y: bottom_right_y + 1]:
-        if (
-            ME in row[top_left_x : bottom_right_x + 1]
-            or ENEMY in row[top_left_x : bottom_right_x + 1]
-        ):
-            return True
-    return False
-
-
-def pruned_legal_moves(board: Board) -> list:
-    """Filters the board to keep the interesting nodes and send them back, so they can be treated"""
-    free_slots = filter(
-        lambda c: c is not None,
-            [
-                (x, y) if board[y][x] == 0 else None
-                for x in range(board.length)
-                for y in range(board.height)
-            ]
-    )
-    legal_moves = []
-    win_moves = []
-    for (col, row) in free_slots:
-        if has_stone_nearby(board, col, row):
-            legal_moves.append((col, row))
-            new_board: Board = deepcopy(board)
-            new_board[row][col] = ME
-            if has_won(new_board, ME):
-                win_moves.append((col, row))
-            new_board[row][col] = ENEMY
-            if has_won(new_board, ENEMY):
-                win_moves.append((col, row))
-
-    return win_moves if win_moves else legal_moves
-
-
 def evaluate(game: Game) -> tuple:
+    print(game.board)
     """Returns the best position to play"""
     # x, y = win_coordinates(game.board, Players['ME'])
     # if x == -1:
@@ -62,4 +18,4 @@ def evaluate(game: Game) -> tuple:
     legal_moves = pruned_legal_moves(game.board)
     if not legal_moves:
         return generate_random_coordinates(game.board.length, game.board.height)
-    return minimax(game.board, legal_moves, player=ME)
+    return minimax(game.board, player=ME)
--- sources/minimax.py
+++ sources/minimax.py
@@ -1,12 +1,81 @@
 """Here is the minimax algorithm implementation, using alpha-beta pruning"""
-
+import random
 from random import choice
 from math import inf
-from .game import Board, ENEMY
+from .game import Board, ENEMY, ME
+from copy import deepcopy
+from .win import has_won
+
+def has_stone_nearby(board: Board, node_x: int, node_y: int) -> bool:
+    """Checks if a node of the board has a stone around it"""
+    top_left_x = node_x - 1 if node_x > 0 else node_x
+    top_left_y = node_y - 1 if node_y > 0 else node_y
+    bottom_right_x = node_x + 1 if node_x < board.length - 1 else node_x
+    bottom_right_y = node_y + 1 if node_y < board.height - 1 else node_y
+
+    for row in board[top_left_y: bottom_right_y + 1]:
+        if (
+            ME in row[top_left_x : bottom_right_x + 1]
+            or ENEMY in row[top_left_x : bottom_right_x + 1]
+        ):
+            return True
+    return False
 
 
-def minimax(board: Board, legal_moves: list, player: int) -> tuple:
+def pruned_legal_moves(board: Board) -> list:
+    """Filters the board to keep the interesting nodes and send them back, so they can be treated"""
+    free_slots = filter(
+        lambda c: c is not None,
+            [
+                (x, y) if board[y][x] == 0 else None
+                for x in range(board.length)
+                for y in range(board.height)
+            ]
+    )
+    legal_moves = []
+    win_moves = []
+    for (col, row) in free_slots:
+        if has_stone_nearby(board, col, row):
+            legal_moves.append((col, row))
+            new_board: Board = deepcopy(board)
+            new_board[row][col] = ME
+            if has_won(new_board, ME):
+                win_moves.append((col, row))
+            new_board[row][col] = ENEMY
+            if has_won(new_board, ENEMY):
+                win_moves.append((col, row))
+
+    return win_moves if win_moves else legal_moves
+
+
+def get_score(board: Board):
+    """Returns the score of a move"""
+    score = random.randint(0, 100)
+    return score
+
+
+def minimax(board: Board, player: int) -> tuple:
     """Minimax algorithm main loop, returns the best move's coordinates"""
-    winner: dict = {"x": -1, "y": -1, "score": -inf if player == ENEMY else inf}
 
-    return choice(legal_moves)
+    def minimax_recur(board: Board, last_move: tuple,  depth : int, player : int) -> dict:
+        if depth == 0:
+            return {"x": last_move[0], "y": last_move[1], "score": get_score(board)}
+        if has_won(board, ME):
+            return {"x": last_move[0], "y": last_move[1], "score": 1000}
+        if has_won(board, ENEMY):
+            return {"x": last_move[0], "y": last_move[1], "score": -1000}
+        legal_moves : list = pruned_legal_moves(board)
+        if not legal_moves:
+            return {"x": last_move[0], "y": last_move[1], "score": get_score(board)}
+
+        moves = []
+        next_turn = ME if player == ENEMY else ENEMY
+        for move in legal_moves:
+            moves.append(minimax_recur(deepcopy(board), move, depth - 1, next_turn))
+        if player == ME:
+            return max(moves, key=lambda i: i['score'])
+        else:
+            return min(moves, key=lambda i: i['score'])
+
+    best_move = minimax_recur(board, (0, 0), 3, ME)
+    return best_move['x'], best_move['y']
